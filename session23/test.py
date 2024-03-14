893353faab8ef47da81e4ed16b108e8416804c0a
Farokhlagha <fl.razeghi@gmail.com>
2024-03-14 09:19:37 +0000

    sudoku

---
+++ session23/test.py
@@ -0,0 +1,28 @@
+# c++
+
+# int x;
+# int a[10]
+# int b[9][9];
+
+#python
+
+x = 2
+x = None
+x = '5'
+
+a = [1, 2, 3]
+
+b = [[None, None, None, None],
+     [None, None, None, None],
+     [None, None, None, None],
+     [None, None, None, None],
+     [None, None, None, None],
+     [None, None, None, None]]
+
+e= [[None for i in range(4)] for j in range(6)]
+
+c= [1, 1, 1, 1]
+d= [1 for i in range(4)]
+
+f = [1, 2, 3, 4, 5, 6, 7]
+g= [i for i in range(1,8)]
